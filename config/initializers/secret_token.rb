98b9b4b09e835ee558342277ed9d47cf364308b2
Shaun + Vincent <shaun+vincent@appacademy.io>
2013-02-01 09:10:01 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecretShareApp::Application.config.secret_token = '0c95ba60a21743ef7c889014bd193f59399f9057fc08c806a2662364bafbe86f4e4bb24e63c1265f1479ab6df422539d952c990fc7993770fe36313bf3364ab7'
